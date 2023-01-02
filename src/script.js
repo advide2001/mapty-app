3e4e3a08286f82e0da21bd482bd988eba49be090
Adarsh Desai <desaiadarsh22@gmail.com>
2023-01-02 07:20:40 +0530

    Fetching user coords using the geolocation API

--- src/script.js
+++ src/script.js
@@ -10,3 +10,23 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Using geo-location api to access the user location data
+
+function getUserLocation() {
+  function success(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(latitude + ' ' + longitude);
+  }
+  function error() {
+    alert('Please allow us to access your location!');
+  }
+
+  if (!navigator.geolocation) {
+    console.log('Geolocation is not supported on your system.');
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+getUserLocation();
